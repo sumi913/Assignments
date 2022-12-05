819ee0108ff4e325b257cee417db95613badcfbb
sumi913 <117143425+sumi913@users.noreply.github.com>
2022-12-05 11:18:18 +0530

    Delete EmailAuth-master (2) directory

--- EmailAuth-master (2)/EmailAuth-master/dashboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from .views import home_view
-
-urlpatterns = [
-    path('', home_view, name="home")
-]
--- EmailAuth-master (2)/EmailAuth-master/teacher/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('addcourse', views.addcourses, name='addcourses'),
-    path('updatecourse', views.updatecourse, name='updatecourse'),
-    path('deletecourse', views.deletecourse, name='deletecourse'),
-
-]
